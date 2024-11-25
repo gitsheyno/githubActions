a58b5f3f79632a513bdf02feb1b9e6c158c68915
shayan heidary <hdrydeveloper@gmail.com>
2024-11-25 12:35:39 +0100

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Counter from "./components/counter/Counter";
-import UserInput from "./components/unerInput/UserInput";
+import UserInput from "./components/userInput/UserInput";
 function App() {
   return (
     <div className="w-full text-center">
